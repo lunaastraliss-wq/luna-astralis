ae347d54b6c976d371d692f2e809b910ce2fd62b
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-01-04 14:43:59 -0500

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import type { Database } from "@/types/supabase"; // optionnel si tu types
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export const supabase = createClientComponentClient<Database>();
