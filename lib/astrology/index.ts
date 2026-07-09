bc7e7f21d52c33084a65c6ecbb0072ca7225adc5
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-09 14:31:53 -0400

    Create index.ts to export astrology constants

---
+++ lib/astrology/index.ts
@@ -0,0 +1,23 @@
+import { SUN } from "./sun";
+import { MOON } from "./moon";
+import { MERCURY } from "./mercury";
+import { VENUS } from "./venus";
+import { MARS } from "./mars";
+import { JUPITER } from "./jupiter";
+import { SATURN } from "./saturn";
+import { URANUS } from "./uranus";
+import { NEPTUNE } from "./neptune";
+import { PLUTO } from "./pluto";
+
+export {
+  SUN,
+  MOON,
+  MERCURY,
+  VENUS,
+  MARS,
+  JUPITER,
+  SATURN,
+  URANUS,
+  NEPTUNE,
+  PLUTO,
+};
