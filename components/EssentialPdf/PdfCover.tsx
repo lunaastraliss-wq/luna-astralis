02e27b9099341bd7e2df964a9bafecf7139a303c
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-10 16:02:06 -0400

    Update PdfCover.tsx

--- components/EssentialPdf/PdfCover.tsx
+++ components/EssentialPdf/PdfCover.tsx
@@ -13,277 +13,310 @@ import PdfPageFooter from "./PdfPageFooter";
 const styles = StyleSheet.create({
   page: {
     position: "relative",
-    paddingTop: 54,
-    paddingBottom: 54,
-    paddingHorizontal: 58,
+    paddingTop: 34,
+    paddingBottom: 42,
+    paddingHorizontal: 52,
     backgroundColor: "#081020",
     color: "#fff8e7",
     fontFamily: "Helvetica",
   },
 
-  topGlow: {
+  glowTop: {
     position: "absolute",
-    top: -160,
-    right: -120,
-    width: 390,
-    height: 390,
-    borderRadius: 195,
-    backgroundColor: "#111d3b",
-    opacity: 0.9,
-  },
-
-  bottomGlow: {
-    position: "absolute",
-    bottom: -190,
-    left: -135,
-    width: 420,
-    height: 420,
-    borderRadius: 210,
-    backgroundColor: "#101a35",
-    opacity: 0.75,
-  },
-
-  orbitOuter: {
-    position: "absolute",
-    top: 107,
-    right: 48,
-    width: 174,
-    height: 174,
-    borderRadius: 87,
-    borderWidth: 0.8,
-    borderColor: "#7c6833",
-    opacity: 0.34,
-  },
-
-  orbitMiddle: {
-    position: "absolute",
-    top: 127,
-    right: 68,
-    width: 134,
-    height: 134,
-    borderRadius: 67,
-    borderWidth: 0.7,
-    borderColor: "#d1aa4d",
-    opacity: 0.24,
-  },
-
-  orbitInner: {
-    position: "absolute",
-    top: 159,
-    right: 100,
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-    borderWidth: 0.7,
-    borderColor: "#f4c95d",
-    opacity: 0.24,
-  },
-
-  starOne: {
-    position: "absolute",
-    top: 104,
-    left: 74,
-    fontSize: 9,
-    color: "#f4c95d",
+    top: -150,
+    right: -110,
+    width: 370,
+    height: 370,
+    borderRadius: 185,
+    backgroundColor: "#162a55",
     opacity: 0.72,
   },
 
-  starTwo: {
+  glowBottom: {
     position: "absolute",
-    top: 218,
-    right: 89,
+    bottom: -190,
+    left: -130,
+    width: 390,
+    height: 390,
+    borderRadius: 195,
+    backgroundColor: "#101c3a",
+    opacity: 0.58,
+  },
+
+  wheelOuter: {
+    position: "absolute",
+    top: 95,
+    right: 34,
+    width: 205,
+    height: 205,
+    borderRadius: 103,
+    borderWidth: 0.8,
+    borderColor: "#d8b858",
+    opacity: 0.2,
+  },
+
+  wheelMiddle: {
+    position: "absolute",
+    top: 117,
+    right: 56,
+    width: 161,
+    height: 161,
+    borderRadius: 81,
+    borderWidth: 0.7,
+    borderColor: "#d8b858",
+    opacity: 0.18,
+  },
+
+  wheelInner: {
+    position: "absolute",
+    top: 148,
+    right: 87,
+    width: 99,
+    height: 99,
+    borderRadius: 50,
+    borderWidth: 0.7,
+    borderColor: "#d8b858",
+    opacity: 0.17,
+  },
+
+  wheelCore: {
+    position: "absolute",
+    top: 185,
+    right: 124,
+    width: 25,
+    height: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: "#f4c95d",
+    opacity: 0.45,
+  },
+
+  decorativeStarOne: {
+    position: "absolute",
+    top: 116,
+    left: 84,
     fontSize: 7,
     color: "#f4c95d",
     opacity: 0.65,
   },
 
-  starThree: {
+  decorativeStarTwo: {
     position: "absolute",
-    bottom: 174,
-    right: 72,
-    fontSize: 8,
+    top: 277,
+    right: 75,
+    fontSize: 6,
     color: "#f4c95d",
     opacity: 0.55,
   },
 
+  decorativeStarThree: {
+    position: "absolute",
+    bottom: 135,
+    right: 66,
+    fontSize: 7,
+    color: "#f4c95d",
+    opacity: 0.45,
+  },
+
   header: {
-    flexDirection: "row",
+    zIndex: 2,
     alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 68,
-    zIndex: 2,
-  },
-
-  logo: {
-    width: 116,
-    height: 46,
-    objectFit: "contain",
-  },
-
-  headerLine: {
-    width: 244,
-    height: 1,
-    backgroundColor: "#47506b",
-  },
-
-  editionLabel: {
-    marginBottom: 18,
-    fontSize: 10,
-    letterSpacing: 3.1,
-    color: "#f4c95d",
-    textTransform: "uppercase",
-  },
-
-  titleBlock: {
-    width: "78%",
-    marginBottom: 38,
-    zIndex: 2,
-  },
-
-  title: {
-    fontSize: 35,
-    lineHeight: 1.12,
-    color: "#fff8e7",
-    marginBottom: 14,
-  },
-
-  titleAccent: {
-    color: "#f4c95d",
-  },
-
-  subtitle: {
-    fontSize: 13,
-    lineHeight: 1.45,
-    color: "#c7cce0",
-    letterSpacing: 0.35,
-  },
-
-  goldDivider: {
-    width: 82,
-    height: 1.5,
-    backgroundColor: "#f4c95d",
     marginBottom: 34,
   },
 
-  identityCard: {
-    position: "relative",
-    borderWidth: 1,
-    borderColor: "#9d8040",
-    backgroundColor: "#111b35",
-    paddingTop: 24,
-    paddingBottom: 22,
-    paddingHorizontal: 26,
-    marginBottom: 30,
-    zIndex: 2,
+  logo: {
+    width: 150,
+    height: 60,
+    objectFit: "contain",
+    marginBottom: 13,
   },
 
-  identityTopLine: {
+  brandLine: {
+    width: 118,
+    height: 1,
+    backgroundColor: "#d4af4e",
+  },
+
+  titleSection: {
+    zIndex: 2,
+    alignItems: "center",
+    marginBottom: 24,
+  },
+
+  reportLabel: {
+    fontSize: 8.5,
+    letterSpacing: 2.8,
+    color: "#f4c95d",
+    textTransform: "uppercase",
+    marginBottom: 15,
+  },
+
+  title: {
+    fontSize: 34,
+    lineHeight: 1.08,
+    color: "#fff8e7",
+    textAlign: "center",
+    marginBottom: 7,
+  },
+
+  titleAccent: {
+    fontSize: 36,
+    color: "#f4c95d",
+    textAlign: "center",
+    marginBottom: 14,
+  },
+
+  subtitle: {
+    width: "78%",
+    fontSize: 10.5,
+    lineHeight: 1.48,
+    color: "#c6cde0",
+    textAlign: "center",
+  },
+
+  identityCard: {
+    zIndex: 2,
+    position: "relative",
+    borderWidth: 1,
+    borderColor: "#b19148",
+    backgroundColor: "#111b35",
+    paddingTop: 18,
+    paddingBottom: 18,
+    paddingHorizontal: 24,
+    marginBottom: 21,
+  },
+
+  identityAccent: {
     position: "absolute",
-    top: 0,
+    top: -1,
     left: 0,
-    width: 72,
+    width: 92,
     height: 2,
     backgroundColor: "#f4c95d",
   },
 
   identityLabel: {
-    fontSize: 8.5,
-    letterSpacing: 2.2,
-    color: "#aab1c8",
+    fontSize: 7.4,
+    letterSpacing: 2.1,
+    color: "#aab2c9",
+    textAlign: "center",
     textTransform: "uppercase",
-    marginBottom: 9,
+    marginBottom: 7,
   },
 
   name: {
     fontSize: 24,
     color: "#fff8e7",
-    marginBottom: 22,
+    textAlign: "center",
+    marginBottom: 15,
   },
 
   dataRow: {
     flexDirection: "row",
     justifyContent: "space-between",
+    borderTopWidth: 0.6,
+    borderTopColor: "#35405d",
+    paddingTop: 12,
   },
 
   dataColumn: {
     width: "31%",
+    alignItems: "center",
   },
 
   dataLabel: {
-    fontSize: 7.5,
-    letterSpacing: 1.45,
-    color: "#8f98b4",
+    fontSize: 6.6,
+    letterSpacing: 1.25,
+    color: "#8993ae",
     textTransform: "uppercase",
-    marginBottom: 6,
+    textAlign: "center",
+    marginBottom: 4,
   },
 
   dataValue: {
-    fontSize: 10.5,
+    fontSize: 9.5,
     color: "#fff8e7",
-    lineHeight: 1.35,
+    lineHeight: 1.25,
+    textAlign: "center",
   },
 
-  introduction: {
-    borderLeftWidth: 2,
-    borderLeftColor: "#f4c95d",
-    paddingLeft: 20,
-    paddingRight: 8,
-    marginBottom: 26,
+  quoteBlock: {
     zIndex: 2,
+    alignItems: "center",
+    paddingHorizontal: 31,
+    marginBottom: 21,
   },
 
-  introText: {
-    fontSize: 11.2,
-    lineHeight: 1.62,
-    color: "#e8e4da",
-    marginBottom: 10,
-  },
-
-  featureRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 28,
-    zIndex: 2,
-  },
-
-  feature: {
-    width: "31.5%",
-    borderTopWidth: 1,
-    borderTopColor: "#46506b",
-    paddingTop: 13,
-  },
-
-  featureNumber: {
-    fontSize: 8,
-    letterSpacing: 1.6,
+  quoteMark: {
+    fontSize: 20,
     color: "#f4c95d",
-    marginBottom: 7,
-  },
-
-  featureTitle: {
-    fontSize: 10.5,
-    color: "#fff8e7",
+    lineHeight: 1,
     marginBottom: 5,
   },
 
-  featureText: {
-    fontSize: 8.2,
-    lineHeight: 1.45,
-    color: "#aeb6ce",
+  quote: {
+    fontSize: 12.5,
+    lineHeight: 1.46,
+    color: "#f1ecdf",
+    textAlign: "center",
+    marginBottom: 7,
   },
 
-  disclaimer: {
-    marginTop: "auto",
-    paddingTop: 14,
-    borderTopWidth: 0.7,
-    borderTopColor: "#39435f",
+  quoteAuthor: {
+    fontSize: 7.4,
+    letterSpacing: 2,
+    color: "#939cb5",
+    textTransform: "uppercase",
+  },
+
+  pillars: {
     zIndex: 2,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderTopWidth: 0.7,
+    borderBottomWidth: 0.7,
+    borderColor: "#3a4562",
+    paddingTop: 13,
+    paddingBottom: 13,
+    marginBottom: 16,
   },
 
-  disclaimerText: {
-    fontSize: 7.8,
-    lineHeight: 1.45,
-    color: "#848da8",
+  pillar: {
+    width: "31%",
+    alignItems: "center",
+  },
+
+  pillarSymbol: {
+    fontSize: 18,
+    color: "#f4c95d",
+    marginBottom: 4,
+  },
+
+  pillarTitle: {
+    fontSize: 8.7,
+    color: "#fff8e7",
+    marginBottom: 3,
+  },
+
+  pillarText: {
+    fontSize: 6.8,
+    color: "#949db6",
+    textAlign: "center",
+  },
+
+  introduction: {
+    zIndex: 2,
+    borderLeftWidth: 1.5,
+    borderLeftColor: "#f4c95d",
+    paddingLeft: 15,
+    paddingRight: 12,
+  },
+
+  introText: {
+    fontSize: 8.6,
+    lineHeight: 1.52,
+    color: "#cfd4e1",
+    textAlign: "justify",
   },
 });
 
@@ -300,45 +333,47 @@ export default function PdfCover({
 }: EssentialPdfProps) {
   return (
     <Page size="A4" style={styles.page}>
-      <View style={styles.topGlow} />
-      <View style={styles.bottomGlow} />
+      <View style={styles.glowTop} />
+      <View style={styles.glowBottom} />
 
-      <View style={styles.orbitOuter} />
-      <View style={styles.orbitMiddle} />
-      <View style={styles.orbitInner} />
+      <View style={styles.wheelOuter} />
+      <View style={styles.wheelMiddle} />
+      <View style={styles.wheelInner} />
+      <View style={styles.wheelCore} />
 
-      <Text style={styles.starOne}>✦</Text>
-      <Text style={styles.starTwo}>✧</Text>
-      <Text style={styles.starThree}>✦</Text>
+      <Text style={styles.decorativeStarOne}>✦</Text>
+      <Text style={styles.decorativeStarTwo}>✧</Text>
+      <Text style={styles.decorativeStarThree}>✦</Text>
 
       <View style={styles.header}>
         <Image src={LOGO_URL} style={styles.logo} />
-        <View style={styles.headerLine} />
+        <View style={styles.brandLine} />
       </View>
 
-      <View style={styles.titleBlock}>
-        <Text style={styles.editionLabel}>
+      <View style={styles.titleSection}>
+        <Text style={styles.reportLabel}>
           Rapport astrologique personnalisé
         </Text>
 
         <Text style={styles.title}>
-          Votre carte du ciel{"\n"}
-          <Text style={styles.titleAccent}>essentielle</Text>
+          Votre carte du ciel
+        </Text>
+
+        <Text style={styles.titleAccent}>
+          essentielle
         </Text>
 
         <Text style={styles.subtitle}>
-          Une lecture élégante et structurée des grandes forces qui
-          composent votre thème natal.
+          Une lecture personnelle des grandes forces qui composent
+          votre thème natal.
         </Text>
       </View>
 
-      <View style={styles.goldDivider} />
-
       <View style={styles.identityCard}>
-        <View style={styles.identityTopLine} />
+        <View style={styles.identityAccent} />
 
         <Text style={styles.identityLabel}>
-          Ce thème astral a été préparé pour
+          Préparé exclusivement pour
         </Text>
 
         <Text style={styles.name}>
@@ -375,52 +410,50 @@ export default function PdfCover({
         </View>
       </View>
 
+      <View style={styles.quoteBlock}>
+        <Text style={styles.quoteMark}>“</Text>
+
+        <Text style={styles.quote}>
+          Chaque naissance possède une signature céleste unique.
+        </Text>
+
+        <Text style={styles.quoteAuthor}>
+          Luna Astralis
+        </Text>
+      </View>
+
+      <View style={styles.pillars}>
+        <View style={styles.pillar}>
+          <Text style={styles.pillarSymbol}>☉</Text>
+          <Text style={styles.pillarTitle}>Soleil</Text>
+          <Text style={styles.pillarText}>
+            Votre identité profonde
+          </Text>
+        </View>
+
+        <View style={styles.pillar}>
+          <Text style={styles.pillarSymbol}>☽</Text>
+          <Text style={styles.pillarTitle}>Lune</Text>
+          <Text style={styles.pillarText}>
+            Votre monde émotionnel
+          </Text>
+        </View>
+
+        <View style={styles.pillar}>
+          <Text style={styles.pillarSymbol}>↑</Text>
+          <Text style={styles.pillarTitle}>Ascendant</Text>
+          <Text style={styles.pillarText}>
+            Votre présence naturelle
+          </Text>
+        </View>
+      </View>
+
       <View style={styles.introduction}>
         <Text style={styles.introText}>
           Au moment précis de votre naissance, le ciel formait une
-          configuration unique. Votre carte du ciel en révèle les
-          grandes lignes et met en lumière votre identité, votre monde
-          émotionnel, votre manière d’aimer, de réfléchir et d’agir.
-        </Text>
-
-        <Text style={styles.introText}>
-          Ce rapport Essentiel vous accompagne dans une première
-          exploration claire et personnelle de votre univers
-          astrologique.
-        </Text>
-      </View>
-
-      <View style={styles.featureRow}>
-        <View style={styles.feature}>
-          <Text style={styles.featureNumber}>01</Text>
-          <Text style={styles.featureTitle}>Vos trois piliers</Text>
-          <Text style={styles.featureText}>
-            Soleil, Lune et Ascendant.
-          </Text>
-        </View>
-
-        <View style={styles.feature}>
-          <Text style={styles.featureNumber}>02</Text>
-          <Text style={styles.featureTitle}>Vos planètes</Text>
-          <Text style={styles.featureText}>
-            Les grandes fonctions de votre personnalité.
-          </Text>
-        </View>
-
-        <View style={styles.feature}>
-          <Text style={styles.featureNumber}>03</Text>
-          <Text style={styles.featureTitle}>Votre équilibre</Text>
-          <Text style={styles.featureText}>
-            Éléments dominants et rythme intérieur.
-          </Text>
-        </View>
-      </View>
-
-      <View style={styles.disclaimer}>
-        <Text style={styles.disclaimerText}>
-          L’astrologie est un outil symbolique de connaissance de soi.
-          Elle révèle des tendances et des possibilités, sans
-          déterminer votre avenir de manière absolue.
+          configuration unique. Ce rapport vous invite à découvrir vos
+          grands piliers astrologiques, vos planètes natales, vos
+          éléments dominants et votre rythme intérieur.
         </Text>
       </View>
 
