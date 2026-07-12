246e4a29d10ee7296345a118f688b4e9f96b3c08
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-12 12:24:29 -0400

    Add PremiumPdfPage component for PDF viewing

---
+++ app/dev/pdf-premium/page.tsx
@@ -0,0 +1,44 @@
+```tsx
+"use client";
+
+import {
+  PDFViewer,
+} from "@react-pdf/renderer";
+
+import PremiumPdfDocument from "@/components/PremiumPdf/PremiumPdfDocument";
+
+export default function PremiumPdfPage() {
+  return (
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+        backgroundColor: "#0b1124",
+      }}
+    >
+      <PDFViewer
+        width="100%"
+        height="100%"
+        style={{
+          border: "none",
+          display: "block",
+        }}
+        showToolbar
+      >
+        <PremiumPdfDocument
+          firstName="Martine"
+          birthDate="17/11/1970"
+          birthTime="21:36"
+          birthCity="Québec, Québec, Canada"
+          planets={[]}
+          angles={{}}
+          wheelImage=""
+        />
+      </PDFViewer>
+    </main>
+  );
+}
+```
