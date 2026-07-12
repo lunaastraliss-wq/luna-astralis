0decc4b9d19112164f178ee8b3809633816ff421
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-12 12:35:09 -0400

    Refactor PremiumPdfPage to use PdfPremiumViewer

--- app/dev/pdf-premium/page.tsx
+++ app/dev/pdf-premium/page.tsx
@@ -1,42 +1,5 @@
-"use client";
+import PdfPremiumViewer from "./PdfPremiumViewer";
 
-import {
-  PDFViewer,
-} from "@react-pdf/renderer";
-
-import PremiumPdfDocument from "@/components/PremiumPdf/PremiumPdfDocument";
-
-export default function PremiumPdfPage() {
-  return (
-    <main
-      style={{
-        width: "100vw",
-        height: "100vh",
-        margin: 0,
-        padding: 0,
-        overflow: "hidden",
-        backgroundColor: "#0b1124",
-      }}
-    >
-      <PDFViewer
-        width="100%"
-        height="100%"
-        style={{
-          border: "none",
-         }}
-        showToolbar
-      >
-        <PremiumPdfDocument
-          firstName="Martine"
-          birthDate="17/11/1970"
-          birthTime="21:36"
-          birthCity="Québec, Québec, Canada"
-          planets={[]}
-          angles={{}}
-          wheelImage=""
-        />
-      </PDFViewer>
-    </main>
-  );
+export default function Page() {
+  return <PdfPremiumViewer />;
 }
-
