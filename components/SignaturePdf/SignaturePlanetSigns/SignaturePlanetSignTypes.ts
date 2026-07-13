25dbc3e504d7619f97e6ee5428564bcc065f6132
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-13 19:31:41 -0400

    Add types for SignaturePlanetSign content and maps

--- components/SignaturePdf/SignaturePlanetSigns/SignaturePlanetSignTypes.ts
+++ components/SignaturePdf/SignaturePlanetSigns/SignaturePlanetSignTypes.ts
@@ -1,1 +1,21 @@
+export type SignaturePlanetSignContent = {
+  energy: string;
 
+  strengths?: string[];
+
+  challenges?: string[];
+
+  evolution?: string;
+
+  quote?: string;
+};
+
+export type SignaturePlanetSignMap = Record<
+  string,
+  SignaturePlanetSignContent
+>;
+
+export type SignaturePlanetSignTexts = Record<
+  string,
+  SignaturePlanetSignMap
+>;
