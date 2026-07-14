a7712feb924ae75dc863bd5f972ea328fda990f3
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-14 12:29:18 -0400

    Remove 'Cancer' from zodiac signs list
    Removed 'Cancer' from the list of zodiac signs.

--- components/SignaturePdf/PdfSignatureStrengths.tsx
+++ components/SignaturePdf/PdfSignatureStrengths.tsx
@@ -84,7 +84,6 @@ const SIGN_NAMES: Record<string, string> = {
   Bélier: "Bélier",
   Taureau: "Taureau",
   Gémeaux: "Gémeaux",
-  Cancer: "Cancer",
   Lion: "Lion",
   Vierge: "Vierge",
   Balance: "Balance",
