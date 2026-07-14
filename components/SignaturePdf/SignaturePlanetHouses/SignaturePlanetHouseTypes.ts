d79a6f2e490c7be260e41a390e9b0c01aea9f0d8
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-13 20:06:55 -0400

    Add types for SignaturePlanetHouse content and texts

--- components/SignaturePdf/SignaturePlanetHouses/SignaturePlanetHouseTypes.ts
+++ components/SignaturePdf/SignaturePlanetHouses/SignaturePlanetHouseTypes.ts
@@ -1,1 +1,13 @@
+export type SignaturePlanetHouseContent = {
+  influence: string;
+};
 
+export type SignatureHousePlanetTexts = Record<
+  string,
+  SignaturePlanetHouseContent
+>;
+
+export type SignaturePlanetHouseTexts = Record<
+  number,
+  SignatureHousePlanetTexts
+>;
