d63a9429c3e87c73e643131943af106b717e65f0
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:29:02 -0400

    Delete app/horoscope/data/belier.ts

--- app/horoscope/data/belier.ts
+++
@@ -1,9 +0,0 @@
-import { createHoroscopeTextes } from "./createHoroscopeTextes";
-
-export const belierHoroscope =
-  createHoroscopeTextes({
-    nom: "Bélier",
-    slug: "belier",
-    element: "Feu",
-    qualite: "initiative",
-  });
