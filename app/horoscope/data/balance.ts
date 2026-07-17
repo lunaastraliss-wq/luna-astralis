58aba465262286c6519fa41f3d84fcc515f3ab69
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:28:49 -0400

    Delete app/horoscope/data/balance.ts

--- app/horoscope/data/balance.ts
+++
@@ -1,9 +0,0 @@
-import { createHoroscopeTextes } from "./createHoroscopeTextes";
-
-export const balanceHoroscope =
-  createHoroscopeTextes({
-    nom: "Balance",
-    slug: "balance",
-    element: "Air",
-    qualite: "harmonie",
-  });
