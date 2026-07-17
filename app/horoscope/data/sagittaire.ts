1aae36d9184a61d5951f04b19d762d4a82ea8564
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:30:35 -0400

    Delete app/horoscope/data/sagittaire.ts

--- app/horoscope/data/sagittaire.ts
+++
@@ -1,9 +0,0 @@
-import { createHoroscopeTextes } from "./createHoroscopeTextes";
-
-export const sagittaireHoroscope =
-  createHoroscopeTextes({
-    nom: "Sagittaire",
-    slug: "sagittaire",
-    element: "Feu",
-    qualite: "expansion",
-  });
