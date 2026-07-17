9cd37183dc6f5c24e5eec0154b5b36f82ed7e58d
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:39:30 -0400

    Add Cancer horoscope data

---
+++ app/horoscope/data/cancer.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const cancerHoroscope =
+  createHoroscopeTextes({
+    nom: "Cancer",
+    slug: "cancer",
+    element: "Eau",
+    qualite: "sensibilité",
+  });
