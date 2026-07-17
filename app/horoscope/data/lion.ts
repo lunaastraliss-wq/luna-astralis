871cad2ca9fe5ff5613acceb3752de859829a1b5
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:39:55 -0400

    Add lion horoscope data file

---
+++ app/horoscope/data/lion.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const lionHoroscope =
+  createHoroscopeTextes({
+    nom: "Lion",
+    slug: "lion",
+    element: "Feu",
+    qualite: "confiance",
+  });
