ede3d1c123d9ea8102d8db77942ede4171606ace
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 16:42:29 -0400

    Create viergeHoroscope with imported texts

---
+++ app/horoscope/data/vierge/index.ts
@@ -0,0 +1,17 @@
+import { energie } from "./energie";
+import { amour } from "./amour";
+import { travail } from "./travail";
+import { finances } from "./finances";
+import { bienEtre } from "./bienEtre";
+import { conseil } from "./conseil";
+
+import type { HoroscopeTextes } from "../types";
+
+export const viergeHoroscope: HoroscopeTextes = {
+  energie,
+  amour,
+  travail,
+  finances,
+  bienEtre,
+  conseil,
+};
