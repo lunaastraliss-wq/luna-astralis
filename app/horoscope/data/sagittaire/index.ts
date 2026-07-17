46b9ab7f766ff60abd801beebd7feff994fb8dbc
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:03:24 -0400

    Create index.ts for Sagittarius horoscope data

---
+++ app/horoscope/data/sagittaire/index.ts
@@ -0,0 +1,17 @@
+import { energie } from "./energie";
+import { amour } from "./amour";
+import { travail } from "./travail";
+import { finances } from "./finances";
+import { bienEtre } from "./bienEtre";
+import { conseil } from "./conseil";
+
+export const sagittaire = {
+  energie,
+  amour,
+  travail,
+  finances,
+  bienEtre,
+  conseil,
+};
+
+export default sagittaire;
