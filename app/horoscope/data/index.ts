6655423da92c559a1a1312be1d4aa747b4366a05
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:22:46 -0400

    Refactor horoscope imports and export structure

--- app/horoscope/data/index.ts
+++ app/horoscope/data/index.ts
@@ -1,71 +1,32 @@
-import type {
-  HoroscopeTextes,
-} from "./types";
+import type { HoroscopeTextes } from "./types";
 
-import {
-  belierHoroscope,
-} from "./belier";
-
-import {
-  taureauHoroscope,
-} from "./taureau";
-
-import {
-  gemeauxHoroscope,
-} from "./gemeaux";
-
-import {
-  cancerHoroscope,
-} from "./cancer";
-
-import {
-  lionHoroscope,
-} from "./lion";
-
-import {
-  viergeHoroscope,
-} from "./vierge";
-
-import {
-  balanceHoroscope,
-} from "./balance";
-
-import {
-  scorpionHoroscope,
-} from "./scorpion";
-
-import {
-  sagittaireHoroscope,
-} from "./sagittaire";
-
-import {
-  capricorneHoroscope,
-} from "./capricorne";
-
-import {
-  verseauHoroscope,
-} from "./verseau";
-
-import {
-  poissonsHoroscope,
-} from "./poissons";
+import belier from "./belier";
+import taureau from "./taureau";
+import gemeaux from "./gemeaux";
+import cancer from "./cancer";
+import lion from "./lion";
+import vierge from "./vierge";
+import balance from "./balance";
+import scorpion from "./scorpion";
+import sagittaire from "./sagittaire";
+import capricorne from "./capricorne";
+import verseau from "./verseau";
+import poissons from "./poissons";
 
 export const HOROSCOPE_TEXTES: Record<
   string,
   HoroscopeTextes
 > = {
-  belier: belierHoroscope,
-  taureau: taureauHoroscope,
-  gemeaux: gemeauxHoroscope,
-  cancer: cancerHoroscope,
-  lion: lionHoroscope,
-  vierge: viergeHoroscope,
-  balance: balanceHoroscope,
-  scorpion: scorpionHoroscope,
-  sagittaire:
-    sagittaireHoroscope,
-  capricorne:
-    capricorneHoroscope,
-  verseau: verseauHoroscope,
-  poissons: poissonsHoroscope,
+  belier,
+  taureau,
+  gemeaux,
+  cancer,
+  lion,
+  vierge,
+  balance,
+  scorpion,
+  sagittaire,
+  capricorne,
+  verseau,
+  poissons,
 };
