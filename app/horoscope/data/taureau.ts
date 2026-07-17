daf2bcc0763caa16cc368f7ecd2ac03c6884f886
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:38:35 -0400

    Add Taureau horoscope data

---
+++ app/horoscope/data/taureau.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const taureauHoroscope =
+  createHoroscopeTextes({
+    nom: "Taureau",
+    slug: "taureau",
+    element: "Terre",
+    qualite: "stabilité",
+  });
