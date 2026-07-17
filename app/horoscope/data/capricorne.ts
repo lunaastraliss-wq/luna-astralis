c1605631c3ea11f03f9fb00ff2fb1d1cc1114938
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:42:01 -0400

    Create capricorne.ts

---
+++ app/horoscope/data/capricorne.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const capricorneHoroscope =
+  createHoroscopeTextes({
+    nom: "Capricorne",
+    slug: "capricorne",
+    element: "Terre",
+    qualite: "discipline",
+  });
