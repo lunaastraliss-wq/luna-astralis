b413210176add07d8f6a9fd25bc5c7abfa6272e1
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:39:00 -0400

    Add gemeauxHoroscope data file

---
+++ app/horoscope/data/gemeaux.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const gemeauxHoroscope =
+  createHoroscopeTextes({
+    nom: "Gémeaux",
+    slug: "gemeaux",
+    element: "Air",
+    qualite: "communication",
+  });
