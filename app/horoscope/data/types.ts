44e078314aff78d452b45fe7eb35e0c1d2630b79
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:36:56 -0400

    Add types for Horoscope data structures

---
+++ app/horoscope/data/types.ts
@@ -0,0 +1,15 @@
+export type HoroscopeTextes = {
+  energie: string[];
+  amour: string[];
+  travail: string[];
+  finances: string[];
+  bienEtre: string[];
+  conseil: string[];
+};
+
+export type HoroscopeSigneData = {
+  nom: string;
+  slug: string;
+  element: string;
+  qualite: string;
+};
