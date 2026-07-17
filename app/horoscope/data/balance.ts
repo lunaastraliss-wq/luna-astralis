af119a9df586fe5f0e433f8e11a74333693c0e77
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:40:41 -0400

    Add balance horoscope data file

---
+++ app/horoscope/data/balance.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const balanceHoroscope =
+  createHoroscopeTextes({
+    nom: "Balance",
+    slug: "balance",
+    element: "Air",
+    qualite: "harmonie",
+  });
