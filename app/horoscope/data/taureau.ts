d376470509f24bb251d6fcfb8b98147e03071d1d
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:31:12 -0400

    Delete app/horoscope/data/taureau.ts

--- app/horoscope/data/taureau.ts
+++
@@ -1,9 +0,0 @@
-import { createHoroscopeTextes } from "./createHoroscopeTextes";
-
-export const taureauHoroscope =
-  createHoroscopeTextes({
-    nom: "Taureau",
-    slug: "taureau",
-    element: "Terre",
-    qualite: "stabilité",
-  });
