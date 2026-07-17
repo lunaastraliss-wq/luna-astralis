dce7613d70102fec8d1dc3096a0bb9c20896e31a
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:25:29 -0400

    Change imports to named imports for horoscope data

--- app/horoscope/data/index.ts
+++ app/horoscope/data/index.ts
@@ -1,17 +1,17 @@
 import type { HoroscopeTextes } from "./types";
 
-import belier from "./belier";
-import taureau from "./taureau";
-import gemeaux from "./gemeaux";
-import cancer from "./cancer";
-import lion from "./lion";
-import vierge from "./vierge";
-import balance from "./balance";
-import scorpion from "./scorpion";
-import sagittaire from "./sagittaire";
-import capricorne from "./capricorne";
-import verseau from "./verseau";
-import poissons from "./poissons";
+import { belier } from "./belier";
+import { taureau } from "./taureau";
+import { gemeaux } from "./gemeaux";
+import { cancer } from "./cancer";
+import { lion } from "./lion";
+import { vierge } from "./vierge";
+import { balance } from "./balance";
+import { scorpion } from "./scorpion";
+import { sagittaire } from "./sagittaire";
+import { capricorne } from "./capricorne";
+import { verseau } from "./verseau";
+import { poissons } from "./poissons";
 
 export const HOROSCOPE_TEXTES: Record<
   string,
