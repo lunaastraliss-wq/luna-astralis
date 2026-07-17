05234e0ff9f94148d752d4e6c7a71670ab17e9f7
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:43:23 -0400

    Create index.ts to export horoscope texts
    This file aggregates horoscope texts for each zodiac sign into a single exportable object.

---
+++ app/horoscope/data/index.ts
@@ -0,0 +1,71 @@
+import type {
+  HoroscopeTextes,
+} from "./types";
+
+import {
+  belierHoroscope,
+} from "./belier";
+
+import {
+  taureauHoroscope,
+} from "./taureau";
+
+import {
+  gemeauxHoroscope,
+} from "./gemeaux";
+
+import {
+  cancerHoroscope,
+} from "./cancer";
+
+import {
+  lionHoroscope,
+} from "./lion";
+
+import {
+  viergeHoroscope,
+} from "./vierge";
+
+import {
+  balanceHoroscope,
+} from "./balance";
+
+import {
+  scorpionHoroscope,
+} from "./scorpion";
+
+import {
+  sagittaireHoroscope,
+} from "./sagittaire";
+
+import {
+  capricorneHoroscope,
+} from "./capricorne";
+
+import {
+  verseauHoroscope,
+} from "./verseau";
+
+import {
+  poissonsHoroscope,
+} from "./poissons";
+
+export const HOROSCOPE_TEXTES: Record<
+  string,
+  HoroscopeTextes
+> = {
+  belier: belierHoroscope,
+  taureau: taureauHoroscope,
+  gemeaux: gemeauxHoroscope,
+  cancer: cancerHoroscope,
+  lion: lionHoroscope,
+  vierge: viergeHoroscope,
+  balance: balanceHoroscope,
+  scorpion: scorpionHoroscope,
+  sagittaire:
+    sagittaireHoroscope,
+  capricorne:
+    capricorneHoroscope,
+  verseau: verseauHoroscope,
+  poissons: poissonsHoroscope,
+};
