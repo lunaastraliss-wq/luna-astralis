dfe2a0a0a549cb7869e22204789caac97cc215fa
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:29:17 -0400

    Delete app/horoscope/data/cancer.ts

--- app/horoscope/data/cancer.ts
+++
@@ -1,9 +0,0 @@
-import { createHoroscopeTextes } from "./createHoroscopeTextes";
-
-export const cancerHoroscope =
-  createHoroscopeTextes({
-    nom: "Cancer",
-    slug: "cancer",
-    element: "Eau",
-    qualite: "sensibilité",
-  });
