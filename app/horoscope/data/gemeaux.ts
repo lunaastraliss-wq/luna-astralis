2205095ff662beb1830aa9bda4dd9c3791cd9bc7
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:29:46 -0400

    Delete app/horoscope/data/gemeaux.ts

--- app/horoscope/data/gemeaux.ts
+++
@@ -1,9 +0,0 @@
-import { createHoroscopeTextes } from "./createHoroscopeTextes";
-
-export const gemeauxHoroscope =
-  createHoroscopeTextes({
-    nom: "Gémeaux",
-    slug: "gemeaux",
-    element: "Air",
-    qualite: "communication",
-  });
