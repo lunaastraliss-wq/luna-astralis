a3e5fa4a45346fa66950860de666d63e535ac334
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:31:40 -0400

    Delete app/horoscope/data/vierge.ts

--- app/horoscope/data/vierge.ts
+++
@@ -1,9 +0,0 @@
-import { createHoroscopeTextes } from "./createHoroscopeTextes";
-
-export const viergeHoroscope =
-  createHoroscopeTextes({
-    nom: "Vierge",
-    slug: "vierge",
-    element: "Terre",
-    qualite: "organisation",
-  });
