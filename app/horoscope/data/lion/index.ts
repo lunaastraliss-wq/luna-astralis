7d123ce1c460b6ebd1535e3b663eff4e7b9e5224
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 16:34:41 -0400

    Add lion horoscope data module

---
+++ app/horoscope/data/lion/index.ts
@@ -0,0 +1,17 @@
+import { energie } from "./energie";
+import { amour } from "./amour";
+import { travail } from "./travail";
+import { finances } from "./finances";
+import { bienEtre } from "./bienEtre";
+import { conseil } from "./conseil";
+
+import type { HoroscopeTextes } from "../types";
+
+export const lionHoroscope: HoroscopeTextes = {
+  energie,
+  amour,
+  travail,
+  finances,
+  bienEtre,
+  conseil,
+};
