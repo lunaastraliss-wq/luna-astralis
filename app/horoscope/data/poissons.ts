3625aad9f8360de828edc1b27983dcbcea7668c3
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:42:55 -0400

    Add Poissons horoscope data

---
+++ app/horoscope/data/poissons.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const poissonsHoroscope =
+  createHoroscopeTextes({
+    nom: "Poissons",
+    slug: "poissons",
+    element: "Eau",
+    qualite: "intuition",
+  });
