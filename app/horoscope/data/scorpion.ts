0f0dd4ca01d66b5d54703026fc33aec02c44517c
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:41:10 -0400

    Add Scorpio horoscope data

---
+++ app/horoscope/data/scorpion.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const scorpionHoroscope =
+  createHoroscopeTextes({
+    nom: "Scorpion",
+    slug: "scorpion",
+    element: "Eau",
+    qualite: "transformation",
+  });
