c9e537c390ccb70dde74bf347fecb4ced1ef4b9b
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:15:49 -0400

    Create index.ts for verseau horoscope data

---
+++ app/horoscope/data/verseau/index.ts
@@ -0,0 +1,17 @@
+import { energie } from "./energie";
+import { amour } from "./amour";
+import { travail } from "./travail";
+import { finances } from "./finances";
+import { bienEtre } from "./bienEtre";
+import { conseil } from "./conseil";
+
+export const verseau = {
+  energie,
+  amour,
+  travail,
+  finances,
+  bienEtre,
+  conseil,
+};
+
+export default verseau;
