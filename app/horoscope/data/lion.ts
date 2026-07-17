607871617eb788cd0a4bc0afbb350db0d41687f5
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 17:30:06 -0400

    Delete app/horoscope/data/lion.ts

--- app/horoscope/data/lion.ts
+++
@@ -1,9 +0,0 @@
-import { createHoroscopeTextes } from "./createHoroscopeTextes";
-
-export const lionHoroscope =
-  createHoroscopeTextes({
-    nom: "Lion",
-    slug: "lion",
-    element: "Feu",
-    qualite: "confiance",
-  });
