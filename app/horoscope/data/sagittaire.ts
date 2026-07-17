5985cd532d36219f1784813872ea0c91fe883b5f
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:41:36 -0400

    Add Sagittarius horoscope data file

---
+++ app/horoscope/data/sagittaire.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const sagittaireHoroscope =
+  createHoroscopeTextes({
+    nom: "Sagittaire",
+    slug: "sagittaire",
+    element: "Feu",
+    qualite: "expansion",
+  });
