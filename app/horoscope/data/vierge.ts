7cc85d4d4bdcfa8536ff29097101a0e027763012
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-17 15:40:17 -0400

    Add Vierge horoscope data

---
+++ app/horoscope/data/vierge.ts
@@ -0,0 +1,9 @@
+import { createHoroscopeTextes } from "./createHoroscopeTextes";
+
+export const viergeHoroscope =
+  createHoroscopeTextes({
+    nom: "Vierge",
+    slug: "vierge",
+    element: "Terre",
+    qualite: "organisation",
+  });
