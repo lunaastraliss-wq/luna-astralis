51d24a2a2987c2271805b56d9f722f4b595a96f9
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-21 10:35:53 -0400

    Refactor PdfHoroscopeViewer component structure

--- app/dev/pdf-horoscope/PdfHoroscopeViewer.tsx
+++ app/dev/pdf-horoscope/PdfHoroscopeViewer.tsx
@@ -1,47 +1,27 @@
-import PdfHoroscopeViewer from "./PdfHoroscopeViewer";
+"use client";
 
-import {
-  buildPremiumDailyHoroscope,
+import dynamic from "next/dynamic";
+
+import type {
+  PremiumDailyHoroscopeResult,
 } from "@/components/HoroscopePdf/buildPremiumDailyHoroscope";
 
-import {
-  calculateNatalChart,
-} from "@/lib/astrology/calculateNatalChart";
+const HoroscopePdfViewer = dynamic(
+  () => import("./HoroscopePdfClientViewer"),
+  {
+    ssr: false,
+  },
+);
 
-const LATITUDE = 46.8139;
-const LONGITUDE = -71.208;
-const TIME_ZONE = "America/Toronto";
+type PdfHoroscopeViewerProps = {
+  horoscope: PremiumDailyHoroscopeResult;
+};
 
-export default function PdfHoroscopeDevPage() {
-  const natalChart = calculateNatalChart({
-    birthDate: "17/11/1970",
-    birthTime: "21:36",
-    latitude: LATITUDE,
-    longitude: LONGITUDE,
-    timeZone: TIME_ZONE,
-  });
-
-  const horoscope = buildPremiumDailyHoroscope({
-    firstName: "Martine",
-
-    zodiacSign: "scorpion",
-    zodiacSignLabel: "Scorpion",
-
-    date: "2026-07-21",
-
-    birthDate: "17/11/1970",
-    birthTime: "21:36",
-    birthCity: "Québec",
-    birthCountry: "Canada",
-
-    latitude: natalChart.latitude,
-    longitude: natalChart.longitude,
-    timeZone: natalChart.timeZone,
-    natalPlanets: natalChart.planets,
-  });
-
+export default function PdfHoroscopeViewer({
+  horoscope,
+}: PdfHoroscopeViewerProps) {
   return (
-    <PdfHoroscopeViewer
+    <HoroscopePdfViewer
       horoscope={horoscope}
     />
   );
