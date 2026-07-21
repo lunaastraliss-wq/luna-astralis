f41414b7961c0008696b7da7f03455ab6a5f5032
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-21 10:37:09 -0400

    Add HoroscopePdfClientViewer component

---
+++ app/dev/pdf-horoscope/HoroscopePdfClientViewer.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  PDFViewer,
+} from "@react-pdf/renderer";
+
+import HoroscopePdf from "@/components/HoroscopePdf/HoroscopePdf";
+
+import type {
+  PremiumDailyHoroscopeResult,
+} from "@/components/HoroscopePdf/buildPremiumDailyHoroscope";
+
+type HoroscopePdfClientViewerProps = {
+  horoscope: PremiumDailyHoroscopeResult;
+};
+
+export default function HoroscopePdfClientViewer({
+  horoscope,
+}: HoroscopePdfClientViewerProps) {
+  return (
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        padding: 0,
+        backgroundColor: "#081020",
+        overflow: "hidden",
+      }}
+    >
+      <PDFViewer
+        width="100%"
+        height="100%"
+        style={{
+          border: "none",
+        }}
+        showToolbar
+      >
+        <HoroscopePdf
+          identity={horoscope.identity}
+          period={horoscope.period}
+          content={horoscope.content}
+        />
+      </PDFViewer>
+    </main>
+  );
+}
