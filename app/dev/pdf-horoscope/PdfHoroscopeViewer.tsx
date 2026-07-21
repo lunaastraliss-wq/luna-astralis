3dc405b8732c8dfd0c58ac451d4fea8bfb8a1c18
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-21 10:34:48 -0400

    Refactor PdfHoroscopeViewer component structure

--- app/dev/pdf-horoscope/PdfHoroscopeViewer.tsx
+++ app/dev/pdf-horoscope/PdfHoroscopeViewer.tsx
@@ -1,8 +1,4 @@
-"use client";
-
-import { PDFViewer } from "@react-pdf/renderer";
-
-import HoroscopePdf from "@/components/HoroscopePdf/HoroscopePdf";
+import PdfHoroscopeViewer from "./PdfHoroscopeViewer";
 
 import {
   buildPremiumDailyHoroscope,
@@ -16,96 +12,37 @@ const LATITUDE = 46.8139;
 const LONGITUDE = -71.208;
 const TIME_ZONE = "America/Toronto";
 
-const natalChart =
-  calculateNatalChart({
-    birthDate:
-      "17/11/1970",
-
-    birthTime:
-      "21:36",
-
-    latitude:
-      LATITUDE,
-
-    longitude:
-      LONGITUDE,
-
-    timeZone:
-      TIME_ZONE,
+export default function PdfHoroscopeDevPage() {
+  const natalChart = calculateNatalChart({
+    birthDate: "17/11/1970",
+    birthTime: "21:36",
+    latitude: LATITUDE,
+    longitude: LONGITUDE,
+    timeZone: TIME_ZONE,
   });
 
-const horoscope =
-  buildPremiumDailyHoroscope({
-    firstName:
-      "Martine",
+  const horoscope = buildPremiumDailyHoroscope({
+    firstName: "Martine",
 
-    zodiacSign:
-      "scorpion",
+    zodiacSign: "scorpion",
+    zodiacSignLabel: "Scorpion",
 
-    zodiacSignLabel:
-      "Scorpion",
+    date: "2026-07-21",
 
-    date:
-      "2026-07-21",
+    birthDate: "17/11/1970",
+    birthTime: "21:36",
+    birthCity: "Québec",
+    birthCountry: "Canada",
 
-    birthDate:
-      "17/11/1970",
-
-    birthTime:
-      "21:36",
-
-    birthCity:
-      "Québec",
-
-    birthCountry:
-      "Canada",
-
-    latitude:
-      natalChart.latitude,
-
-    longitude:
-      natalChart.longitude,
-
-    timeZone:
-      natalChart.timeZone,
-
-    natalPlanets:
-      natalChart.planets,
+    latitude: natalChart.latitude,
+    longitude: natalChart.longitude,
+    timeZone: natalChart.timeZone,
+    natalPlanets: natalChart.planets,
   });
 
-export default function PdfHoroscopeViewer() {
   return (
-    <main
-      style={{
-        width: "100vw",
-        height: "100vh",
-        margin: 0,
-        padding: 0,
-        backgroundColor:
-          "#081020",
-        overflow: "hidden",
-      }}
-    >
-      <PDFViewer
-        width="100%"
-        height="100%"
-        style={{
-          border: "none",
-        }}
-        showToolbar
-      >
-        <HoroscopePdf
-          identity={
-            horoscope.identity
-          }
-          period={
-            horoscope.period
-          }
-          content={
-            horoscope.content
-          }
-        />
-      </PDFViewer>
-    </main>
+    <PdfHoroscopeViewer
+      horoscope={horoscope}
+    />
   );
 }
