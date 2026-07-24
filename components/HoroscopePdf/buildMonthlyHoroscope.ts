9112890926ea979ee79fcce6151487facc06bf7d
lunaastraliss-wq <lunaastraliss@gmail.com>
2026-07-24 12:14:09 -0400

    Update import path for buildMonthlyPlanetaryInfluences

--- components/HoroscopePdf/buildMonthlyHoroscope.ts
+++ components/HoroscopePdf/buildMonthlyHoroscope.ts
@@ -14,7 +14,7 @@ import {
 
 import {
   buildMonthlyPlanetaryInfluences,
-} from "./buildMonthlyPlanetaryInfluences";
+} from "./month/buildMonthlyPlanetaryInfluences";
 
 /*
 |--------------------------------------------------------------------------
